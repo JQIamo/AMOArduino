e3341f738bb5d42b156b62ca503df13dbcbf617e
Neal Pisenti (UMD) <npisenti@umd.edu>
2013-02-26 09:06:13 -0500

    untested code done on SimpleLCD

--- SimpleLCD/SimpleLCD.h
+++ SimpleLCD/SimpleLCD.h
@@ -1,3 +1,24 @@
+/* 
+   SimpleLCD.h - wrapper library for interfacing with SparkFun SerialLCD
+   Created by Neal Pisenti, 2013.
+   JQI - Strontium - UMD
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   aunsigned long with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+ */
+
 #ifndef SimpleLCD_h
 #define SimpleLCD_h
 
@@ -20,5 +41,11 @@ class SimpleLCD
         void underlineCursorOff();
         void boxCursorOn();
         void boxCursorOff();
-        int backlight(int);
+        void backlight(int);
 
+    private:
+        HardwareSerial* _lcd
+
+}
+
+#endif
